5984626d8437ab9559fa6b20d26520d433175d02
mrxYan6 <21052021@hdu.edu.cn>
2023-04-15 11:43:40 +0800

    finish simulation

--- RegisterFile/RegisterFile.srcs/sim_1/new/simu_RegFile.v
+++ RegisterFile/RegisterFile.srcs/sim_1/new/simu_RegFile.v
@@ -5,18 +5,39 @@ module simu_RegFile();
     reg [4:0]A_addr,B_addr,W_Addr;
     wire[31:0]A_out,B_out;
     reg rst_,clk_W;
-    Register_File rf(32'd233,1'b1,rst_,clk_W,A_addr,B_addr,W_addr,A_out,B_out);
+    reg [31:0]data_write;
+    Register_File rf(data_write,1'b1,rst_,clk_W,A_addr,B_addr,W_Addr,A_out,B_out);
 
     integer x;
     initial begin
-        rst_ <= 1;
+        rst_ = 1;
+        clk_W = 1;
+        A_addr = 0;
+        B_addr = 0;
+        W_Addr = 0;
         #10
-        rst_ <= 0;
-        #10 rst_ <= 1;
+        rst_ = 0;
+        #10 rst_ = 1;
+        
+        for(x = 0;x < 32; x = x + 1)begin
+        #5
+        A_addr = x;
+        end
+
+        for(x = 0;x < 32; x = x + 1)begin
+            W_Addr = x;
+            B_addr = x;
+            data_write  = x + 233;
+            clk_W = 1;
+            #2
+            clk_W = 0;
+            #2 
+            clk_W = 1;
+        end
 
         for(x = 0;x < 32; x = x + 1)begin
         #5
-        A_addr <= x;
+        A_addr = x;
         end
     end
 endmodule
--- RegisterFile/RegisterFile.srcs/sources_1/new/TOP.v
+++ RegisterFile/RegisterFile.srcs/sources_1/new/TOP.v
@@ -1,16 +1,13 @@
 `timescale 1ns / 1ps
-module TOP(A_addr,B_addr,W_Addr,ALU_OP,Reg_Write,clk_100M,rst_,clk_Read,clk_F,clk_Write,AN,Seg,FR);
+module BigBigALU(A_addr,B_addr,W_Addr,ALU_OP,Reg_Write,clk_100M,rst_,clk_Read,clk_F,clk_Write,F,FR);
     input [4:0]A_addr,B_addr,W_Addr;
     input [3:0]ALU_OP;
     input Reg_Write,clk_100M;
     input rst_,clk_Read,clk_F,clk_Write;
-    output [7:0]AN;
-    output [7:0]Seg;
     output [3:0]FR;
-
+    output [31:0]F;
 
     wire [31:0]Reg_A,Reg_B;
-    wire [31:0]A,B,F;
 
     Register_File rgf(
         .rst_(rst_),
@@ -31,10 +28,36 @@ module TOP(A_addr,B_addr,W_Addr,ALU_OP,Reg_Write,clk_100M,rst_,clk_Read,clk_F,cl
         .clk_A(clk_Read),
         .clk_B(clk_Read),
         .clk_F(clk_F),
-        .A(A),
-        .B(B),
+        .F(F),
+        .FR(FR));
+endmodule
+
+
+
+module TOP(A_addr,B_addr,W_Addr,ALU_OP,Reg_Write,clk_100M,rst_,clk_Read,clk_F,clk_Write,AN,Seg,FR);
+    input [4:0]A_addr,B_addr,W_Addr;
+    input [3:0]ALU_OP;
+    input Reg_Write,clk_100M;
+    input rst_,clk_Read,clk_F,clk_Write;
+    output [7:0]AN;
+    output [7:0]Seg;
+    output [3:0]FR;
+
+    wire [31:0]F;
+    
+    BigBigALU aluu(
+        .A_addr(A_addr),
+        .B_addr(B_addr),
+        .W_Addr(W_Addr),
+        .ALU_OP(ALU_OP),
+        .Reg_Write(Reg_Write),
+        .clk_100M(clk_100M),
+        .rst_(rst_),
+        .clk_Read(clk_Read),
+        .clk_F(clk_F),
+        .clk_Write(clk_Write),
         .F(F),
         .FR(FR));
 
-    scan_data tube(rst_,Reg_A,clk_100M,AN,Seg);
+    scan_data tube(rst_,F,clk_100M,AN,Seg);
 endmodule
